cd216fe562c7ccd18365ddebd5d3b730453eacfc
LaNamandine <34181920+LaNamandine@users.noreply.github.com>
2019-03-10 19:15:15 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -65,7 +65,7 @@
       }
       // Sign in with email and pass.
       // [START createwithemail]
-      firebase.auth().createUserWithEmailAndPassword(email, password, score).catch(function(error) {
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -77,6 +77,15 @@
         }
         console.log(error);
         // [END_EXCLUDE]
+	sendEmailVerification;
+	
+	       firebase.database().ref('users/' + userId).set({
+    username: pseudo,
+    email: email,
+    score : score
+  });
+
+})
       });
       // [END createwithemail]
     }
